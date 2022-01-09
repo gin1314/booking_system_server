f59ede917605d8b4de362c956f65f7dd3832bf84
Eugene Santos <eugene.santos@smartchartsfx.com>
2022-01-09 16:31:30 +0800

    - fix

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('phone_no')->unique();
-            $table->enum('role', ['admin', 'engineer', 'client'])->unique();
+            $table->enum('role', ['admin', 'engineer', 'client']);
             $table->string('address');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
